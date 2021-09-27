d5cb1f632f40dd96d7551cecb6618a430017729a
pradeepx <pradeep.suthar@blitzscale.co>
2021-09-27 21:51:24 +0530

    click listner added to all the items

--- app/src/main/java/com/seven/myapp/MainActivity.kt
+++ app/src/main/java/com/seven/myapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.seven.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.Observer
@@ -12,15 +13,26 @@ import androidx.lifecycle.ViewModelProvider
 import com.seven.myapp.databinding.ActivityMainBinding
 import com.seven.myapp.response.HomeBannersResponse
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    MainAdapter.SquareBannerViewHolder.ItemClickedSquare,
+    MainAdapter.RectangularBannerViewHolder.ItemClickedRect,
+    MainAdapter.TextViewHolder.TextViewClicked,
+    MainAdapter.ImageViewHolder.ItemImageClicked,
+    MainAdapter.SuperUserViewHolder.ItemClickedSuperUser,
+    MainAdapter.CoverBannerViewHolder.ItemClickedCover,
+    MainAdapter.UserViewHolder.ItemClickedUser,
+    MainAdapter.StripBannerViewHolder.ItemClickedStrip {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MainAdapter
     private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        adapter = MainAdapter(this)
+        adapter = MainAdapter(this,this, this,
+            this, this, this, this,
+            this, this)
+
         binding.rvList.adapter = adapter
 
 
@@ -28,17 +40,48 @@ class MainActivity : AppCompatActivity() {
         val viewModelFactory = MainViewModelFactory(repository = repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
         viewModel.getBannersList()
-        viewModel.liveData.observe(this, Observer { response->
-            if(response.isSuccessful){
-                Toast.makeText(this,"success to fetch data",Toast.LENGTH_LONG).show()
+        viewModel.liveData.observe(this, Observer { response ->
+            if (response.isSuccessful) {
+                Toast.makeText(this, "success to fetch data", Toast.LENGTH_LONG).show()
                 response.body()?.let {
-                adapter.setData(it as ArrayList<HomeBannersResponse.HomeBannersResponseItem>)
+                    adapter.setData(it as ArrayList<HomeBannersResponse.HomeBannersResponseItem>)
                 }
-            }
-            else{
-                Toast.makeText(this,"failed to fetch data",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "failed to fetch data", Toast.LENGTH_LONG).show()
 
             }
         })
+    }
+
+    override fun itemClickedRect(view: View) {
+        Toast.makeText(this,"Rectangle Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemClickedCover(view: View) {
+        Toast.makeText(this,"Cover View Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemClickedSquare(view: View) {
+        Toast.makeText(this,"Square Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemClickedStrip(view: View) {
+        Toast.makeText(this,"Strip Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemClickedUser(view: View) {
+        Toast.makeText(this,"User Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemClickedSuperUser(view: View) {
+        Toast.makeText(this,"Super User Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun itemImageClicked(view: View) {
+        Toast.makeText(this,"Image Clicked",Toast.LENGTH_LONG).show()
+    }
+
+    override fun textViewClicked(view: View) {
+        Toast.makeText(this,"Text Clicked",Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/seven/myapp/MainAdapter.kt
+++ app/src/main/java/com/seven/myapp/MainAdapter.kt
@@ -2,6 +2,7 @@ package com.seven.myapp
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -9,9 +10,17 @@ import com.seven.myapp.databinding.*
 import com.seven.myapp.response.HomeBannersResponse
 
 class MainAdapter(
-    val context: Context
+    val context: Context,
+    private var clickedSquare: SquareBannerViewHolder.ItemClickedSquare,
+    private var clickedRect: RectangularBannerViewHolder.ItemClickedRect,
+    private var textClicked: TextViewHolder.TextViewClicked,
+    private var imageClicked: ImageViewHolder.ItemImageClicked,
+    private var clickedSuperUser: SuperUserViewHolder.ItemClickedSuperUser,
+    private var clickedCover: CoverBannerViewHolder.ItemClickedCover,
+    private var clickedUser: UserViewHolder.ItemClickedUser,
+    private var clickedStrip: StripBannerViewHolder.ItemClickedStrip,
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var banners = ArrayList<HomeBannersResponse.HomeBannersResponseItem>()
+    var banners = ArrayList<HomeBannersResponse.HomeBannersResponseItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
@@ -98,20 +107,17 @@ class MainAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (getItemViewType(position)) {
-            SQUARE_BANNER -> (holder as SquareBannerViewHolder).bind(banners[position], position)
-            RECTANGULAR_BANNER -> (holder as RectangularBannerViewHolder).bind(
-                banners[position],
-                position
-            )
-            STRIP_BANNER -> (holder as StripBannerViewHolder).bind(banners[position], position)
-            LINE_BANNER -> (holder as SeparatorLineViewHolder).bind(banners[position], position)
-            COVER_BANNER -> (holder as CoverBannerViewHolder).bind(banners[position], position)
-            USER_BANNER -> (holder as UserViewHolder).bind(banners[position], position)
-            SUPER_BANNER -> (holder as SuperUserViewHolder).bind(banners[position], position)
-            TEXT_BANNER -> (holder as TextViewHolder).bind(banners[position], position)
-            IMAGE_BANNER -> (holder as ImageViewHolder).bind(banners[position], position)
-            NA_BANNER -> (holder as NaViewHolder).bind(banners[position], position)
-            else -> (holder as NaViewHolder).bind(banners[position], position)
+            SQUARE_BANNER -> (holder as SquareBannerViewHolder).bind(banners[position], clickedSquare)
+            RECTANGULAR_BANNER -> (holder as RectangularBannerViewHolder).bind(banners[position], clickedRect)
+            STRIP_BANNER -> (holder as StripBannerViewHolder).bind(banners[position], clickedStrip)
+            LINE_BANNER -> (holder as SeparatorLineViewHolder).bind(banners[position])
+            COVER_BANNER -> (holder as CoverBannerViewHolder).bind(banners[position], clickedCover)
+            USER_BANNER -> (holder as UserViewHolder).bind(banners[position],clickedUser)
+            SUPER_BANNER -> (holder as SuperUserViewHolder).bind(banners[position],clickedSuperUser)
+            TEXT_BANNER -> (holder as TextViewHolder).bind(banners[position],textClicked)
+            IMAGE_BANNER -> (holder as ImageViewHolder).bind(banners[position],imageClicked)
+            NA_BANNER -> (holder as NaViewHolder).bind(banners[position])
+            else -> (holder as NaViewHolder).bind(banners[position])
 
         }
     }
@@ -140,118 +146,209 @@ class MainAdapter(
         notifyDataSetChanged()              /// <----cat
     }
 
-    class RectangularBannerViewHolder(private val binding: ItemRectangularBannerBinding, private val context: Context) :
+    class RectangularBannerViewHolder(
+        private val binding: ItemRectangularBannerBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         var background = binding.ivAvatar
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener: ItemClickedRect) {
+            banner.img?.let {
+                Glide.with(context).load(it).into(background)
+            }
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedRect(it)
+            }
+
+        }
+
+        interface ItemClickedRect {
+            fun itemClickedRect(view: View)
+        }
+    }
+
+    class CoverBannerViewHolder(
+        private val binding: ItemCoverBannerBinding,
+        private val context: Context
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        var background = binding.ivAvatar
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener: ItemClickedCover) {
+            banner.img?.let {
+                Glide.with(context).load(it).into(background)
+            }
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedCover(it)
+            }
+
+
+        }
+
+        interface ItemClickedCover {
+            fun itemClickedCover(view: View)
+        }
+    }
+
+    class SquareBannerViewHolder(
+        private val binding: ItemSquareBannerBinding,
+        private val context: Context
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        //        var text = binding.tvText
+//        var itemClickListner: ItemClickedSquare?=null
+        var background = binding.ivAvatar
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener: ItemClickedSquare) {
+//            text.text = banner.text
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedSquare(it)
+            }
             banner.img?.let {
                 Glide.with(context).load(it).into(background)
             }
         }
 
-    }
-    class CoverBannerViewHolder(private val binding: ItemCoverBannerBinding, private val context: Context) :
-        RecyclerView.ViewHolder(binding.root) {
-        var background = binding.ivAvatar
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
-            banner.img?.let {
-                Glide.with(context).load(it).into(background)
-            }
+        interface ItemClickedSquare {
+            fun itemClickedSquare(view: View)
         }
+
     }
-        class SquareBannerViewHolder(private val binding: ItemSquareBannerBinding,private val context: Context) :
+
+    class StripBannerViewHolder(
+        private val binding: ItemStripBannerBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
-//        var text = binding.tvText
+        //        var text = binding.tvText
         var background = binding.ivAvatar
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener:ItemClickedStrip) {
 //            text.text = banner.text
             banner.img?.let {
-                Glide.with(context).load(it).into(background)
 
+                Glide.with(context).load(it).into(background)
             }
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedStrip(it)
+            }
+        }
+
+        interface ItemClickedStrip {
+            fun itemClickedStrip(view: View)
         }
 
     }
 
-    class StripBannerViewHolder(private val binding: ItemStripBannerBinding, private val context: Context) :
-        RecyclerView.ViewHolder(binding.root) {
-//        var text = binding.tvText
-        var background = binding.ivAvatar
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
-//            text.text = banner.text
-            banner.img?.let {
-
-                Glide.with(context).load(it).into(background)
-            }
-        }
-
-    }
-
-    class SeparatorLineViewHolder(private val binding: ItemLineSepratorBinding, private val context: Context) :
+    class SeparatorLineViewHolder(
+        private val binding: ItemLineSepratorBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         var background = binding.ivAvatar
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem) {
             banner.img?.let {
                 Glide.with(context).load(it).into(background)
             }
         }
     }
 
-    class UserViewHolder(private val binding: ItemUserProfileBinding, private val context: Context) :
+    class UserViewHolder(
+        private val binding: ItemUserProfileBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         var dp = binding.profileImage
         var username = binding.username
         var bio = binding.bio
 
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener:ItemClickedUser) {
             banner.img?.let {
                 Glide.with(context).load(it).into(dp)
             }
             username.text = banner.text
             bio.text = banner.color
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedUser(it)
+            }
+
         }
+
+        interface ItemClickedUser {
+            fun itemClickedUser(view: View)
+        }
+
     }
-    class SuperUserViewHolder(private val binding: ItemSuperUserProfileBinding, private val context: Context) :
+
+    class SuperUserViewHolder(
+        private val binding: ItemSuperUserProfileBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         var dp = binding.profileImage
         var username = binding.username
         var bio = binding.bio
 
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener:ItemClickedSuperUser) {
             banner.img?.let {
                 Glide.with(context).load(it).into(dp)
             }
             username.text = banner.text
             bio.text = banner.color
+            binding.root.setOnClickListener {
+                itemClickListener.itemClickedSuperUser(it)
+            }
+
         }
+
+        interface ItemClickedSuperUser {
+            fun itemClickedSuperUser(view: View)
+        }
+
     }
 
-    class ImageViewHolder(private val binding: ItemImageBannerBinding, private val context: Context) :
+    class ImageViewHolder(
+        private val binding: ItemImageBannerBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         var img = binding.ivImage
 
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, itemClickListener: ItemImageClicked) {
             banner.img?.let {
                 Glide.with(context).load(it).into(img)
             }
+            binding.root.setOnClickListener {
+                itemClickListener.itemImageClicked(it)
+            }
+
         }
+
+        interface ItemImageClicked {
+            fun itemImageClicked(view: View)
+        }
+
     }
 
     class TextViewHolder(private val binding: ItemTextBinding, private val context: Context) :
         RecyclerView.ViewHolder(binding.root) {
         var textView = binding.tvText
 
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem,itemClickListener:TextViewClicked) {
             textView.text = banner.text
+            binding.root.setOnClickListener {
+                itemClickListener.textViewClicked(it)
+            }
+
+        }
+
+        interface TextViewClicked {
+            fun textViewClicked(view: View)
         }
     }
 
     class NaViewHolder(private val binding: ItemNaBinding, private val context: Context) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem, pos: Int) {
+        fun bind(banner: HomeBannersResponse.HomeBannersResponseItem) {
         }
     }
-
 
 
 }
